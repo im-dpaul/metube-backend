5f6b7bba00f9e12202d036cfe2a21d8e04eeed2c
Debjyoti Paul <debjyoti.paul@grorapid.com>
2024-10-22 18:03:48 +0530

    cover image & cloudinary bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,7 +13,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
@@ -21,7 +21,15 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required");
@@ -52,7 +60,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   return res
-    .status(20)
+    .status(201)
     .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,11 +11,11 @@ const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
 
-    const response = cloudinary.uploader.upload(localFilePath, {
+    const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
 
-    console.log("file uploaded", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);
